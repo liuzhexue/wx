a3c2547cc8632b41661657e9f8a25d7a6b65f598
LZX <598623261@qq.com>
2018-01-05 13:44:58 +0800

    111

---
+++ weilong/pages/index/index.js
@@ -0,0 +1,45 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '../imgs/1.jpg',
+      '../imgs/2.jpg',
+      '../imgs/3.jpg',
+    ],
+    logoUrls: [
+      '../imgs/logo1.png',
+      '../imgs/logo2.png',
+      '../imgs/logo3.png',
+      '../imgs/logo4.png',
+      '../imgs/logo5.png',
+      '../imgs/logo6.png',
+      '../imgs/logo7.png',
+      '../imgs/logo8.png',
+      '../imgs/logo9.png',
+      '../imgs/logo10.png',
+    ],
+    productUrls: [
+      '../imgs/list1.png',
+      '../imgs/list2.png',
+      '../imgs/list3.png',
+      '../imgs/list4.png',
+      ],
+    discountListUrls:[
+      '../imgs/lists1.png',
+      '../imgs/lists2.png',
+      '../imgs/lists3.png',
+      '../imgs/lists4.png',
+    ],
+    footerimgUrls:[
+      '../imgs/footer.png',
+      '../imgs/logo.png',
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+})
